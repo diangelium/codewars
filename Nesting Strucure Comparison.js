8d1c6aba4705959137273bf87bf97e359950bf33
Ihor Sokolov <diangelium@gmail.com>
2017-03-19 18:58:16 +0200

    a

--- Nesting Strucure Comparison.js
+++ Nesting Strucure Comparison.js
@@ -1,16 +1,22 @@
-var result = ''; 
+Array.prototype.sameStructureAs = function (other) {
+  if(!Array.isArray(other) && this.length != other.length) return false;
+  
+  var result = '';
+	
+  function inner(arr, result) {
 
-function inner(arr) {
-
- for (var i = 0; i < arr.length; i++) {
+   for (var i = 0; i < arr.length; i++) {
+      
     if (Array.isArray(arr[i])) {
-    result += arr[i].length + ':';
-        if(arr[i].length != 0) inner(arr[i]);
-    }
-
-    else result += 'N';
-
+      result += arr[i].length + ':';
+      arr[i].length != 0 ? inner(arr[i], result) : result += ';';
+    } 
+    result += 'N';
   }
  
    return result;
+}
+
+  
+  return inner(this) == inner(other) ? true : false;
 }
